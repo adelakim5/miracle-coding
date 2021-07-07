f69185927114d5e8544fdeb23c0e5a7c3df9c5ad
adelakim <diddnjs02@likelion.org>
2021-07-07 14:35:53 +0900

    modify: Image 문제 해결
    - this를 바인딩 해줌으로써 해결
    - Image의 render 함수의 this가 Image로 걸리지 않았던 문제
    - 추정컨대 eventController 함수 안에 들어가고, 거기서 실행될 때 this를 잘못 잡은거 아닐까 싶음
    - this는 run-time에 결정되니까...
    - 해결법
        - this 바인딩 해주기
        - render를 화살표 함수로 바꾸기

--- src/pages/components/Image.js
+++ src/pages/components/Image.js
@@ -8,7 +8,7 @@ class Image {
   }
 
   setState() {
-    const { throttle } = eventController(this.setData, this.render);
+    const { throttle } = eventController(this.setData, this.render.bind(this)); // this로 bind를 안해주면 Image가 this로 잡히지 않음. 왜일까?
     throttle();
   }
 
@@ -18,7 +18,12 @@ class Image {
   };
 
   render() {
-    this.$ul.innerText = localStorage.getItem("imagesData");
+    // 화살표 함수로 하면 this가 바로 상위에, 그렇지 않으면 undefined를 잡음. 근데 class 내부인데 대체 왜?
+    // console.log(this);
+    this.$ul.innerText = "";
+    const $imageData = document.createElement("div");
+    $imageData.innerText = localStorage.getItem("imagesData");
+    this.$ul.append($imageData);
   }
 }
 
